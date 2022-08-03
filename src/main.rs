11e8e5c7948860f3b37bbf9cc9839a533e4cf290
JB-it <56448520+JB-it@users.noreply.github.com>
2022-08-03 16:43:11 +0200

    Added more controls and refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use egui::Pos2;
 use macroquad::prelude::*;
-use std::{thread, time};
-use std::sync::{Arc, Mutex};
 use std::mem;
+use std::sync::{Arc, Mutex};
+use std::{thread, time};
 
 #[derive(Clone)]
 struct SimulationConfig {
     pub board_size: IVec2,
-    pub hovered: bool,  
+    pub hovered: bool,
     pub auto_run: bool,
     pub should_simulate_next_frame: bool,
     pub wait_time: f32,
@@ -15,6 +15,8 @@ struct SimulationConfig {
     pub erasing: bool,
     pub draw_borders: bool,
     pub error: bool,
+    pub alive_color: Color,
+    pub dead_color: Color,
 }
 
 impl SimulationConfig {
@@ -28,7 +30,9 @@ impl SimulationConfig {
             drawing: false,
             erasing: false,
             draw_borders: true,
-            error: false
+            error: false,
+            alive_color: BLACK,
+            dead_color: WHITE,
         }
     }
 }
@@ -44,6 +48,7 @@ struct Board {
 struct Rules {
     pub birth: Vec<bool>,
     pub survive: Vec<bool>,
+    pub adding_lifetime: i8,
 }
 
 impl Board {
@@ -67,12 +72,18 @@ impl Board {
         let index = (y * self.cx + x) as usize;
         self.states[index] = state;
     }
+
+    pub fn lower_cell_lifetime(&mut self, x: i32, y: i32, amount: i8) {
+        let index = (y * self.cx + x) as usize;
+        self.states[index] -= amount;
+        self.states[index] = self.states[index].max(0);
+    }
 }
 
 #[macroquad::main("Cellular Automata")]
 async fn main() {
     let game_mtx = Arc::new(Mutex::new(Board::new(const_ivec2!([26, 24]))));
-    
+
     //Initial board setup
     let mut c_game = game_mtx.lock().unwrap();
 
@@ -85,44 +96,68 @@ async fn main() {
     let rules = Rules {
         birth: vec![false, false, false, true, false, false, false, false, false],
         survive: vec![false, false, true, true, false, false, false, false, false],
+        adding_lifetime: 1,
     };
 
     let mut cell_below_mouse = IVec2::new(0, 0);
 
-    let mut simulation_cfg = SimulationConfig::new(
-        const_ivec2!([26, 20])
-    );
+    let mut simulation_cfg = SimulationConfig::new(const_ivec2!([26, 20]));
 
     let mut handler = thread::spawn(|| {});
 
-    loop {        
-        clear_background(WHITE);
-
+    loop {
         egui_macroquad::ui(|egui_ctx| {
             let window = egui::Window::new("Configurations");
-            let response = window.show(egui_ctx, |ui| {
+            let response = window
+                .show(egui_ctx, |ui| {
                     simulation_cfg.error = false;
                     ui.label("Simulation Settings");
                     if ui.button("Next step").clicked() {
                         simulation_cfg.should_simulate_next_frame = true;
                     }
                     ui.checkbox(&mut simulation_cfg.auto_run, "Running");
-                    ui.add(egui::Slider::new(&mut simulation_cfg.wait_time, 3f32..=100f32).text("Tick time"));
+                    ui.add(
+                        egui::Slider::new(&mut simulation_cfg.wait_time, 0f32..=100f32)
+                            .text("Tick time"),
+                    );
                     ui.label("Cell controls");
                     ui.checkbox(&mut simulation_cfg.drawing, "Draw Cells");
                     ui.checkbox(&mut simulation_cfg.erasing, "Erase Cells");
+                    if ui.button("Clear board").clicked() {
+                        let mut c_game = game_mtx.lock().unwrap();
+                        c_game.states = vec![0; (c_game.cx * c_game.cy) as usize];
+                        drop(c_game);
+                    }
+                    if ui.button("Randomize Field").clicked() {
+                        let mut c_game = game_mtx.lock().unwrap();
+                        for i in 0..(c_game.cx * c_game.cy) as usize {
+                            c_game.states[i] = if rand::rand() % 2 == 0 { 1 } else { 0 };
+                        }
+                        drop(c_game);
+                    }
                     ui.label("Board Controls");
-                    ui.add(egui::Slider::new(&mut simulation_cfg.board_size.x, 1..=500).text("Board Width"));
-                    ui.add(egui::Slider::new(&mut simulation_cfg.board_size.y, 1..=500).text("Board Height"));
-                    ui.checkbox(&mut simulation_cfg.draw_borders, "Draw borders");                   
+                    ui.add(
+                        egui::Slider::new(&mut simulation_cfg.board_size.x, 1..=500)
+                            .text("Board Width"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut simulation_cfg.board_size.y, 1..=500)
+                            .text("Board Height"),
+                    );
+                    ui.checkbox(&mut simulation_cfg.draw_borders, "Draw borders");
                     //Error handling
                     if simulation_cfg.drawing && simulation_cfg.erasing {
                         ui.label("You can't draw and erase at the same time");
                         simulation_cfg.error = true;
                     }
-                }).unwrap().response;
+                })
+                .unwrap()
+                .response;
 
-            simulation_cfg.hovered = response.rect.contains(Pos2::new(mouse_position().0 as f32, mouse_position().1 as f32));
+            simulation_cfg.hovered = response.rect.contains(Pos2::new(
+                mouse_position().0 as f32,
+                mouse_position().1 as f32,
+            ));
         });
 
         //Goes to next step if an error happened
@@ -146,12 +181,16 @@ async fn main() {
 
             cell_below_mouse = IVec2::new(x, y);
         }
-        
+
         //Resizes the board if the user changes the size of the board
         if game.cx != simulation_cfg.board_size.x || game.cy != simulation_cfg.board_size.y {
-            let _ = mem::replace(&mut *game, Board::new(const_ivec2!([simulation_cfg.board_size.x, simulation_cfg.board_size.y])));
-    
-            //game = Board::new(const_ivec2!([simulation_cfg.board_size.x, simulation_cfg.board_size.y]));
+            let _ = mem::replace(
+                &mut *game,
+                Board::new(const_ivec2!([
+                    simulation_cfg.board_size.x,
+                    simulation_cfg.board_size.y
+                ])),
+            );
         }
 
         //Drawing and erasing
@@ -180,7 +219,9 @@ async fn main() {
             }
         }
 
-        draw_board(&game, &simulation_cfg.draw_borders);
+        clear_background(simulation_cfg.dead_color);
+
+        draw_board(&game, &simulation_cfg);
 
         egui_macroquad::draw();
 
@@ -210,10 +251,10 @@ fn next_step(game: &mut Board, rules: &Rules) {
                 if rules.survive[neighbors as usize] {
                     new_game_state.set_cell_at_position(x, y, 1);
                 } else {
-                    new_game_state.set_cell_at_position(x, y, 0);
+                    new_game_state.lower_cell_lifetime(x, y, 1);
                 }
             } else if rules.birth[neighbors as usize] {
-                new_game_state.set_cell_at_position(x, y, 1);
+                new_game_state.set_cell_at_position(x, y, rules.adding_lifetime.clone());
             } else {
                 new_game_state.set_cell_at_position(x, y, 0);
             }
@@ -223,7 +264,7 @@ fn next_step(game: &mut Board, rules: &Rules) {
     game.states = new_game_state.states.clone();
 }
 
-fn draw_board(game: &Board, borders_drawn: &bool) {
+fn draw_board(game: &Board, simulation_cfg: &SimulationConfig) {
     let wx = screen_width() / game.cx as f32;
     let wy = screen_height() / game.cy as f32;
 
@@ -232,16 +273,23 @@ fn draw_board(game: &Board, borders_drawn: &bool) {
             let px = x as f32 * wx;
             let py = y as f32 * wy;
 
-            if *borders_drawn {
+            if simulation_cfg.draw_borders {
                 if game.states[(x + y * game.cx) as usize] == 0 {
-                    draw_rectangle_lines(px as f32, py as f32, wx, wy, 3.0, BLACK);
+                    draw_rectangle_lines(
+                        px as f32,
+                        py as f32,
+                        wx,
+                        wy,
+                        3.0,
+                        simulation_cfg.alive_color,
+                    );
                 } else {
-                    draw_rectangle(px as f32, py as f32, wx, wy, BLACK);
+                    draw_rectangle(px as f32, py as f32, wx, wy, simulation_cfg.alive_color);
                 }
             } else if game.states[(x + y * game.cx) as usize] == 0 {
                 //draw_rectangle(px as f32, py as f32, wx, wy,  WHITE);
             } else {
-                draw_rectangle(px as f32, py as f32, wx, wy, BLACK);
+                draw_rectangle(px as f32, py as f32, wx, wy, simulation_cfg.alive_color);
             }
 
             //Draws the rectangle on the Macroquad window
