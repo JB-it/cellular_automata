3e97b6680ca0925a156004982af127c285ae46ca
JB-it <56448520+JB-it@users.noreply.github.com>
2022-08-03 10:34:31 +0200

    Beginning to use Multithreading and Mutexes

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
 use egui::Pos2;
 use macroquad::prelude::*;
+use std::{thread::{self, JoinHandle}, time};
+use std::sync::{Arc, Mutex};
+use std::mem;
 
-//const BOARD_SIZE: IVec2 = const_ivec2!([26, 20]);
-
-struct Board_Config {
+#[derive(Clone)]
+struct SimulationConfig {
     pub board_size: IVec2,
     pub hovered: bool,  
     pub auto_run: bool,
+    pub should_simulate_next_frame: bool,
+    pub wait_time: f32,
     pub drawing: bool,
-    pub erasing: bool, 
+    pub erasing: bool,
+    pub draw_borders: bool,
     pub error: bool,
 }
 
-impl Board_Config {
+impl SimulationConfig {
     pub fn new(board_size: IVec2) -> Self {
         Self {
             board_size,
             hovered: false,
             auto_run: false,
+            should_simulate_next_frame: false,
+            wait_time: 0.0,
             drawing: false,
             erasing: false,
+            draw_borders: true,
             error: false
         }
     }
 }
 
+#[derive(Clone)]
 struct Board {
     pub cx: i32,
     pub cy: i32,
     pub states: Vec<i8>,
 }
 
+#[derive(Clone)]
 struct Rules {
     pub birth: Vec<bool>,
     pub survive: Vec<bool>,
@@ -61,25 +71,67 @@ impl Board {
 
 #[macroquad::main("Cellular Automata")]
 async fn main() {
-    let mut game: Board = Board::new(const_ivec2!([26, 24]));
+    let game_mtx = Arc::new(Mutex::new(Board::new(const_ivec2!([26, 24]))));
+    
+    let mut c_game = game_mtx.lock().unwrap();
 
     let rules = Rules {
         birth: vec![false, false, false, true, false, false, false, false, false],
         survive: vec![false, false, true, true, false, false, false, false, false],
     };
 
-    game.set_cell_at_position(1, 1, 1);
-    game.set_cell_at_position(2, 1, 1);
-    game.set_cell_at_position(3, 1, 1);
+    c_game.set_cell_at_position(1, 1, 1);
+    c_game.set_cell_at_position(2, 1, 1);
+    c_game.set_cell_at_position(3, 1, 1);
+
+    drop(c_game);
 
     let mut cell_below_mouse = IVec2::new(0, 0);
 
-    let mut window_cfg = Board_Config::new(
+    let mut simulation_cfg = SimulationConfig::new(
         const_ivec2!([26, 20])
     );
 
-    loop {
+    let mut handler = thread::spawn(|| {});
+
+    loop {        
         clear_background(WHITE);
+
+        egui_macroquad::ui(|egui_ctx| {
+            let window = egui::Window::new("Configurations");
+            let response = window.show(egui_ctx, |ui| {
+                    simulation_cfg.error = false;
+                    ui.label("Simulation Settings");
+                    if ui.button("Next step").clicked() {
+                        simulation_cfg.should_simulate_next_frame = true;
+                    }
+                    ui.checkbox(&mut simulation_cfg.auto_run, "Running");
+                    ui.add(egui::Slider::new(&mut simulation_cfg.wait_time, 3f32..=100f32).text("Tick time"));
+                    ui.label("Cell controls");
+                    ui.checkbox(&mut simulation_cfg.drawing, "Draw Cells");
+                    ui.checkbox(&mut simulation_cfg.erasing, "Erase Cells");
+                    ui.label("Board Controls");
+                    ui.add(egui::Slider::new(&mut simulation_cfg.board_size.x, 1..=500).text("Board Width"));
+                    ui.add(egui::Slider::new(&mut simulation_cfg.board_size.y, 1..=500).text("Board Height"));
+                    ui.checkbox(&mut simulation_cfg.draw_borders, "Draw borders");                   
+                    //Error handling
+                    if simulation_cfg.drawing && simulation_cfg.erasing {
+                        ui.label("You can't draw and erase at the same time");
+                        simulation_cfg.error = true;
+                    }
+                }).unwrap().response;
+
+            simulation_cfg.hovered = response.rect.contains(Pos2::new(mouse_position().0 as f32, mouse_position().1 as f32));
+        });
+
+        //Goes to next step if an error happened
+        if simulation_cfg.error {
+            egui_macroquad::draw();
+            next_frame().await;
+            continue;
+        }
+
+        let mut game = game_mtx.lock().unwrap();
 
         if is_mouse_button_down(MouseButton::Left) {
             let fx = mouse_position().0 as f32;
@@ -93,56 +145,41 @@ async fn main() {
 
             cell_below_mouse = IVec2::new(x, y);
         }
-
-        egui_macroquad::ui(|egui_ctx| {
-            let mut window = egui::Window::new("Configurations");
-            let mut response = window.show(egui_ctx, |ui| {
-                    window_cfg.error = false;
-                    ui.label("Simulation Settings");
-                    if ui.button("Next step").clicked() {
-                        next_step(&mut game, &rules);
-                    }
-                    ui.checkbox(&mut window_cfg.auto_run, "Running");
-                    ui.label("Cell controls");
-                    ui.checkbox(&mut window_cfg.drawing, "Draw Cells");
-                    ui.checkbox(&mut window_cfg.erasing, "Erase Cells");
-                    ui.label("Board Controls");
-                    ui.add(egui::Slider::new(&mut window_cfg.board_size.x, 1..=100).text("Board Width"));
-                    ui.add(egui::Slider::new(&mut window_cfg.board_size.y, 1..=100).text("Board Height"));
-                                        
-                    //Error handling
-                    if window_cfg.drawing && window_cfg.erasing {
-                        ui.label("You can't draw and erase at the same time");
-                        window_cfg.error = true;
-                    }
-                }).unwrap().response;
-
-            window_cfg.hovered = response.rect.contains(Pos2::new(mouse_position().0 as f32, mouse_position().1 as f32));
-        });
-
-        if game.cx != window_cfg.board_size.x || game.cy != window_cfg.board_size.y {
-            game = Board::new(const_ivec2!([window_cfg.board_size.x, window_cfg.board_size.y]));
+        
+        //Resizes the board if the user changes the size of the board
+        if game.cx != simulation_cfg.board_size.x || game.cy != simulation_cfg.board_size.y {
+            let _ = mem::replace(&mut *game, Board::new(const_ivec2!([simulation_cfg.board_size.x, simulation_cfg.board_size.y])));
+    
+            //game = Board::new(const_ivec2!([simulation_cfg.board_size.x, simulation_cfg.board_size.y]));
         }
 
-        if window_cfg.error {
-            egui_macroquad::draw();
-            next_frame().await;
-            continue;
-        }
-
-        if is_mouse_button_down(MouseButton::Left) && !window_cfg.hovered {
-            if window_cfg.drawing {
+        //Drawing and erasing
+        if is_mouse_button_down(MouseButton::Left) && !simulation_cfg.hovered {
+            if simulation_cfg.drawing {
                 game.set_cell_at_position(cell_below_mouse.x, cell_below_mouse.y, 1);
-            } else if window_cfg.erasing {
+            } else if simulation_cfg.erasing {
                 game.set_cell_at_position(cell_below_mouse.x, cell_below_mouse.y, 0);
             }
         }
 
-        if(window_cfg.auto_run) {
-            next_step(&mut game, &rules);
+        //Runs the simulation automatically
+        if simulation_cfg.auto_run || simulation_cfg.should_simulate_next_frame {
+            simulation_cfg.should_simulate_next_frame = false;
+
+            if handler.is_finished() {
+                let sm_cfg = simulation_cfg.clone();
+                let mut game_for_thread = game_mtx.clone();
+                let c_rules = rules.clone();
+                handler = thread::spawn(move || {
+                    let time = sm_cfg.wait_time;
+                    thread::sleep(time::Duration::from_millis((time * 10.0) as u64));
+                    let mut c_game = game_for_thread.lock().unwrap();
+                    next_step(&mut c_game, &c_rules);
+                });
+            }
         }
 
-        draw_board(&game);
+        draw_board(&game, &simulation_cfg.draw_borders);
 
         egui_macroquad::draw();
 
@@ -168,18 +205,16 @@ fn next_step(game: &mut Board, rules: &Rules) {
                 }
             }
 
-            if game.get_cell_at_position(x, y) == 1 {
+            if game.get_cell_at_position(x, y) > 0 {
                 if rules.survive[neighbors as usize] {
                     new_game_state.set_cell_at_position(x, y, 1);
                 } else {
                     new_game_state.set_cell_at_position(x, y, 0);
                 }
+            } else if rules.birth[neighbors as usize] {
+                new_game_state.set_cell_at_position(x, y, 1);
             } else {
-                if rules.birth[neighbors as usize] {
-                    new_game_state.set_cell_at_position(x, y, 1);
-                } else {
-                    new_game_state.set_cell_at_position(x, y, 0);
-                }
+                new_game_state.set_cell_at_position(x, y, 0);
             }
         }
     }
@@ -187,7 +222,7 @@ fn next_step(game: &mut Board, rules: &Rules) {
     game.states = new_game_state.states.clone();
 }
 
-fn draw_board(game: &Board) {
+fn draw_board(game: &Board, borders_drawn: &bool) {
     let wx = screen_width() / game.cx as f32;
     let wy = screen_height() / game.cy as f32;
 
@@ -196,8 +231,14 @@ fn draw_board(game: &Board) {
             let px = x as f32 * wx;
             let py = y as f32 * wy;
 
-            if game.states[(x + y * game.cx) as usize] == 0 {
-                draw_rectangle_lines(px as f32, py as f32, wx, wy, 3.0, BLACK);
+            if *borders_drawn {
+                if game.states[(x + y * game.cx) as usize] == 0 {
+                    draw_rectangle_lines(px as f32, py as f32, wx, wy, 3.0, BLACK);
+                } else {
+                    draw_rectangle(px as f32, py as f32, wx, wy, BLACK);
+                }
+            } else if game.states[(x + y * game.cx) as usize] == 0 {
+                //draw_rectangle(px as f32, py as f32, wx, wy,  WHITE);
             } else {
                 draw_rectangle(px as f32, py as f32, wx, wy, BLACK);
             }
